1ce62d08946e2c029d2092d271bd8f4cc02987f5
open-circuits-robotics <owlrobotics5271@gmail.com>
2024-11-06 15:50:22 -0600

    hi

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,6 @@
+package frc.robot;
+
+public class Constants {
+    //in order: front left x & y, front right, back left, back right.
+    public static final double[] swerveOffsets = {2, 2, 2, -2, -2, -2, -2, 2};
+}
--- src/main/java/frc/robot/Subsystems/SwerveDriveSubsystem.java
+++ src/main/java/frc/robot/Subsystems/SwerveDriveSubsystem.java
@@ -1,23 +1,26 @@
 package frc.robot.Subsystems;
 
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.Constants;
 
 public class SwerveDriveSubsystem extends SubsystemBase {
     // Motors/things to control
-    public Spark flDrive = new Spark(0);
-    public Spark frDrive = new Spark(1);
-    public Spark rlDrive = new Spark(2);
-    public Spark rrDrive = new Spark(3);
-    public DifferentialDrive differentialDrive = new DifferentialDrive(flDrive, frDrive);
+    SwerveDriveKinematics kinematics;
 
     // Constructor
     public SwerveDriveSubsystem() {
 
-        flDrive.addFollower(rlDrive);
-        frDrive.addFollower(rrDrive);
+        kinematics = new SwerveDriveKinematics(
+            new Translation2d(Constants.swerveOffsets[0], Constants.swerveOffsets[1]),
+            new Translation2d(Constants.swerveOffsets[2], Constants.swerveOffsets[3]),
+            new Translation2d(Constants.swerveOffsets[4], Constants.swerveOffsets[5]),
+            new Translation2d(Constants.swerveOffsets[6], Constants.swerveOffsets[7])
+        );
 
     }
     public void periodic() {
